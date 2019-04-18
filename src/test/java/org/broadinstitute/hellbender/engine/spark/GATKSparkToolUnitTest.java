a089ccede31f758a2019038cb4b6c2f2f7b4f52e
Chris Norman <cnorman@broadinstitute.org>
2019-04-18 08:04:05 -0400

    VCF header line refactoring changes.

--- src/test/java/org/broadinstitute/hellbender/engine/spark/GATKSparkToolUnitTest.java
+++ src/test/java/org/broadinstitute/hellbender/engine/spark/GATKSparkToolUnitTest.java
@@ -40,6 +40,11 @@ public class GATKSparkToolUnitTest extends GATKBaseTest {
         VCFHeaderLine sourceLine = hdr.getOtherHeaderLine("source");
         Assert.assertEquals(sourceLine.getValue(), tool.getClass().getSimpleName());
 
+        // TODO: this is bogus:
+        //  1) this isn't the name of the key anymore...
+        //  2) there can be more than one GATKCommandLine header lines (or at least it should
+        //     be possible to have more than one)
+        //  3) this header line is not really guaranteed to be a VCFIDHeaderLine, although it is
         VCFIDHeaderLine commandLine = (VCFIDHeaderLine) hdr.getOtherHeaderLine("GATKCommandLine");
         Assert.assertEquals(commandLine.getID(), tool.getClass().getSimpleName());
 
