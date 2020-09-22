f94e05ef5eb215e4a9807c36e68465536b26f733
kcibul <kcibul@broadinstitute.org>
2020-09-22 15:46:16 -0400

    Ability to filter variants based on QC in ArrayExtractCohort  (#6844)
    * incorporate QC data into extract

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/arrays/ArrayExtractCohortEngine.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/arrays/ArrayExtractCohortEngine.java
@@ -10,6 +10,7 @@ import org.broadinstitute.hellbender.engine.ProgressMeter;
 import org.broadinstitute.hellbender.engine.ReferenceDataSource;
 import org.broadinstitute.hellbender.tools.variantdb.SampleList;
 import org.broadinstitute.hellbender.tools.variantdb.arrays.BasicArrayData.ArrayGenotype;
+import org.broadinstitute.hellbender.tools.variantdb.CommonCode;
 import org.broadinstitute.hellbender.tools.variantdb.SchemaUtils;
 import org.broadinstitute.hellbender.tools.variantdb.arrays.tables.ProbeInfo;
 import org.broadinstitute.hellbender.tools.variantdb.arrays.tables.ProbeQcMetrics;
