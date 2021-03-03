99efb18245b26327de3ca64af4e96902fddeec37
Morgan Taylor <marymorg@broadinstitute.org>
2021-03-03 16:08:38 -0500

    try to filter only variants for vet table

--- src/main/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/CreateVariantIngestFiles.java
+++ src/main/java/org/broadinstitute/hellbender/tools/variantdb/nextgen/CreateVariantIngestFiles.java
@@ -86,9 +86,9 @@ public final class CreateVariantIngestFiles extends VariantWalker {
             optional = true)
     public CommonCode.ModeEnum mode = CommonCode.ModeEnum.EXOMES;
 
-    @Argument(fullName = "output-type", 
-            shortName = "ot", 
-            doc = "[Experimental] Output file format: TSV, ORC or PARQUET [default=TSV].", 
+    @Argument(fullName = "output-type",
+            shortName = "ot",
+            doc = "[Experimental] Output file format: TSV, ORC or PARQUET [default=TSV].",
             optional = true)
     public CommonCode.OutputType outputType = CommonCode.OutputType.TSV;
 
@@ -188,7 +188,7 @@ public final class CreateVariantIngestFiles extends VariantWalker {
 
 
         // create VET output
-        if (!variant.isReferenceBlock()) {
+        if (!variant.isReferenceBlock() && variant.isVariant()) {  // verify with Laura that reblockGVCF can have a variant for ./.
             vetTsvCreator.apply(variant, readsContext, referenceContext, featureContext);
         }
         try {
